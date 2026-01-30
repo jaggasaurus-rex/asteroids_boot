e84ded868306089b1f4a21244a42efb645e27a51
jaggasaurus-rex <jaggers.tyler@gmail.com>
2026-01-30 14:51:24 -0800

    Frame Rate Limiter

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     print("Starting Asteroids with pygame version: "+str(pygame.version.ver))
     print("Screen width: "+str(SCREEN_WIDTH))
@@ -15,8 +17,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
         pygame.display.flip()
+        
+        dt = clock.tick(60)/1000
 
 
 
