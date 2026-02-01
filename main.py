42cc6a29219cf45083f293d3edbf366ce3388b71
jaggasaurus-rex <jaggers.tyler@gmail.com>
2026-01-31 17:05:51 -0800

    Finally drawing something

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
 from constants import (SCREEN_HEIGHT, SCREEN_WIDTH)
 from logger import log_state
+from player import Player
+from circleshape import CircleShape
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player_location = Player(x, y)
     
     print("Starting Asteroids with pygame version: "+str(pygame.version.ver))
     print("Screen width: "+str(SCREEN_WIDTH))
@@ -19,9 +24,14 @@ def main():
                 return
         
         screen.fill("black")
+        player_location.draw(screen)
         pygame.display.flip()
         
         dt = clock.tick(60)/1000
+
+
+    
+
 
 
 
